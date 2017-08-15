efc18389d6d1fcf122d4d904f66a5d6eb2d5e489
Mohamed K. Eid <mohamedkeid@gmail.com>
2017-08-14 20:02:00 -0700

    updated blog page

--- src/js/components/blog.js
+++ src/js/components/blog.js
@@ -4,9 +4,15 @@ import { connect } from "react-redux"
 
 class Post extends Component {
     render() {
+        const date = this.props.date.toLocaleDateString('en-US', {
+            day : 'numeric',
+            month : 'short',
+            year : 'numeric'
+        })
         return (
             <div className="post">
                 <div className="title">{this.props.title}</div>
+                <div className="date">By Mohamed Eid on {date}</div>
                 <img src={this.props.imgSrc} />
                 <div className="preview">{this.props.preview}</div>
             </div>
@@ -24,13 +30,14 @@ class BlogSite extends Component {
             <Post
                 key={post.title}
                 title={post.title}
+                date={post.date}
                 imgSrc={post.imgSrc}
                 preview={post.preview}
             />
         )
         return (
             <div className="blog-site">
-                {posts}
+                <div className="container">{posts}</div>
             </div>
         )
     }
--- src/js/reducers/posts.js
+++ src/js/reducers/posts.js
@@ -1,9 +1,10 @@
 const PostsReducer = () => ([
     {
         title: "Hello World!",
-        date: new Date(),
-        imgSrc: "",
-        preview: "testing.."
+        date: new Date(2017, 8, 14),
+        imgSrc: "/images/posts/post1.jpg",
+        preview: "This post inaugurates the launch of my personal website and portfolio. I'm pretty happy with how it turned out and content with all the experience I've acquired through the building process. It took me a 4 days to build from scratch using Node, React, and Redux. More is to come in terms of expansion hopefully very soon.",
+        content: null
     }
 ])
 
