862f7db09ed9089ca0d24ef0c16822eb918cc7a7
Mohamed K. Eid <mohamedkeid@gmail.com>
2017-08-20 14:07:59 -0700

    improved css animations for portfolio project items

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -13,11 +13,18 @@ class PortfolioItem extends Component {
             delay: props.delay,
             isVisible: false
         }
+
+        // Bind this to functions
+        this.handleMouseEnter = this.handleMouseEnter.bind(this)
         this.makeVisible = this.makeVisible.bind(this)
     }
 
     componentDidMount() {
         setTimeout(this.makeVisible, this.state.delay)
+    }
+
+    handleMouseEnter() {
+        this.props.focus(this.props.title)
     }
 
     makeVisible() {
@@ -27,25 +34,30 @@ class PortfolioItem extends Component {
     }
 
     render() {
+        const ghostClass = this.props.isGhost ? " ghost" : ""
         const visibleClass = this.state.isVisible ? " visible" : ""
-        const className = "portfolio-item" + visibleClass
+        const className = "portfolio-item" + ghostClass + visibleClass
 
         return (
-            <li className={className}>
-                <a href={this.props.url}>
-                    <img src={this.props.imgSrc} />
-                    <div className="portfolio-item-info">
-                        <div className="portfolio-item-title">
-                            {this.props.title}
+            <li className="portfolio-item-container"
+                onMouseEnter={this.handleMouseEnter}
+                onMouseLeave={this.props.removeGhosts}>
+                <div className={className}>
+                    <a href={this.props.url}>
+                        <img src={this.props.imgSrc} />
+                        <div className="portfolio-item-info">
+                            <div className="portfolio-item-title">
+                                {this.props.title}
+                            </div>
+                            <div className="portfolio-item-type">
+                                {this.props.type}
+                            </div>
                         </div>
-                        <div className="portfolio-item-type">
-                            {this.props.type}
+                        <div className="portfolio-arrow">
+                            >
                         </div>
-                    </div>
-                    <div className="portfolio-arrow">
-                        >
-                    </div>
-                </a>
+                    </a>
+                </div>
             </li>
         )
     }
@@ -57,10 +69,42 @@ class PortfolioItem extends Component {
 * @extends Component
 */
 class Portfolio extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ghostProjects: []
+        }
+
+        // Bing this to functions
+        this.focusOnItem = this.focusOnItem.bind(this)
+        this.removeGhosts = this.removeGhosts.bind(this)
+    }
+
+    focusOnItem(focusedProject) {
+        this.setState({
+            ghostProjects: this.props.projects.filter(
+                project => project.title !== focusedProject
+            ).map(project => project.title)
+        })
+    }
+
+    removeGhosts() {
+        this.setState({
+            ghostProjects: []
+        })
+    }
+
     render() {
+        const appearDelay = 150
         const portfolioItems = this.props.projects.map(
             (item, index) =>
-            <PortfolioItem key={item.title} delay={index * 100} {...item} />
+            <PortfolioItem
+                key={item.title}
+                delay={index * appearDelay}
+                focus={this.focusOnItem}
+                isGhost={this.state.ghostProjects.includes(item.title)}
+                removeGhosts={this.removeGhosts}
+                {...item} />
         )
 
         return (
